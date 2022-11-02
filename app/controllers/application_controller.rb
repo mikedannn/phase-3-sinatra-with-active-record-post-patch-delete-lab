fb4fe675df794001417bd6802d0f83d0f44b3130
Michael <michael.daniels1013@gmail.com>
2022-11-02 13:40:15 -0500

    commit tree f4bbd1e7c6e6a3ea68aa10577880e8131fd266e5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,14 @@ class ApplicationController < Sinatra::Base
     bakery.to_json(include: :baked_goods)
   end
 
+  patch '/bakeries/:id' do 
+    bakery = Bakery.find(params[:id])
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json
+  end
+
   get '/baked_goods/by_price' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_goods = BakedGood.by_price
@@ -23,4 +31,18 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  delete '/baked_goods/:id' do 
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
 end
